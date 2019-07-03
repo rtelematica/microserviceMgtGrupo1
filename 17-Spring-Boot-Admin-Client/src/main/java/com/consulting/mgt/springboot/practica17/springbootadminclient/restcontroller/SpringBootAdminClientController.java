f817610d38c812f192829bd9393eb2d10c81720e
Ivan Garcia <isc.ivgarcia@gmail.com>
2019-07-03 15:38:10 -0500

    Clase 7 - Pracitca 17 Spring Boot Admin

--- 17-Spring-Boot-Admin-Client/src/main/java/com/consulting/mgt/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
+++ 17-Spring-Boot-Admin-Client/src/main/java/com/consulting/mgt/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
@@ -1,8 +1,23 @@
 package com.consulting.mgt.springboot.practica17.springbootadminclient.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RestController
 public class SpringBootAdminClientController {
 
+	@Value("${spring.application.name}")
+	private String appName;
+
+	@GetMapping
+	public String hello() {
+
+		log.debug("sending hello!");
+
+		return "Hello I'm " + appName + "!!!";
+	}
 }

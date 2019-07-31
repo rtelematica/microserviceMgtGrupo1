1324cf5d99520cfa4b0267b19804f4664e9bb013
Ivan Garcia <isc.ivgarcia@gmail.com>
2019-07-31 01:54:46 -0500

    practica 25, 26 and ppt updates

---
+++ 25-Sentence-Microservice/src/main/java/com/consulting/mgt/springboot/practica25/hystrix/sentencemicroservice/client/IAdjectiveServiceClient.java
@@ -0,0 +1,6 @@
+package com.consulting.mgt.springboot.practica25.hystrix.sentencemicroservice.client;
+
+public interface IAdjectiveServiceClient {
+
+	public String getAdjective();
+}
---
+++ 25-Sentence-Microservice/src/main/java/com/consulting/mgt/springboot/practica25/hystrix/sentencemicroservice/client/impl/AdjectiveServiceClient.java
@@ -0,0 +1,42 @@
+package com.consulting.mgt.springboot.practica25.hystrix.sentencemicroservice.client.impl;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.consulting.mgt.springboot.practica25.hystrix.sentencemicroservice.client.IAdjectiveServiceClient;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class AdjectiveServiceClient implements IAdjectiveServiceClient {
+
+	@Value("${adjective-microservice.service-name:adjective-microservice}")
+	private String serviceName;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Override
+	@SneakyThrows
+	// Define comando Hystrix
+	public String getAdjective() {
+		URI uri = new URI(String.format("http://%s/word", serviceName));
+
+		log.info("calling service URI {}", uri.toURL());
+
+		return restTemplate.getForObject(uri, String.class);
+	}
+
+	public String defaultAdjective(Throwable ex) {
+		log.info("Fallback method entered, exception {}: {}", ex.getClass().getSimpleName(), ex.getMessage());
+		log.error("exception", ex);
+		return "(some adjective)";
+	}
+
+}

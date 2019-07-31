d9800d0a37f894b780e40490dd1aa9f07862c79d
Ivan Garcia <isc.ivgarcia@gmail.com>
2019-07-31 10:57:13 -0500

    Clase 10 - Practica 24 Feign

--- 24-Age-Microservice/src/main/java/com/consulting/mgt/springboot/practica24/feign/agemicroservice/client/IRandomServiceClient.java
+++ 24-Age-Microservice/src/main/java/com/consulting/mgt/springboot/practica24/feign/agemicroservice/client/IRandomServiceClient.java
@@ -2,9 +2,14 @@ package com.consulting.mgt.springboot.practica24.feign.agemicroservice.client;
 
 import java.util.Map;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 // Agrega Feign Client
+@FeignClient(name="random-microservice", path="random-service")
 public interface IRandomServiceClient {
 
 	// Agrega "/next" edpoint mediante Metodo GET.
+	@GetMapping("/next")
 	public Map<String, Object> next();
 }
--- 24-Users-Microservice/src/main/java/com/consulting/mgt/springboot/practica24/feign/usersmicroservice/client/IAgeServiceClient.java
+++ 24-Users-Microservice/src/main/java/com/consulting/mgt/springboot/practica24/feign/usersmicroservice/client/IAgeServiceClient.java
@@ -2,9 +2,14 @@ package com.consulting.mgt.springboot.practica24.feign.usersmicroservice.client;
 
 import java.util.Map;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 //Agrega Feign Client
+@FeignClient(name="age-microservice", path="age-service")
 public interface IAgeServiceClient {
 
 	// Agrega "/age" edpoint mediante Metodo GET.
+	@GetMapping("/age")
 	public Map<String, Object> age();
 }
